e673eb9b64a985e69c920c8fcd3a2293507a1c95
Vince Au <vinceau09@gmail.com>
2015-12-08 12:28:45 +1100

    Add part 2 of day 3 solution

--- src/d3.py
+++ src/d3.py
@@ -4,7 +4,15 @@ import sys
 
 xpos = 0
 ypos = 0
-visited = set() #empty set
+visited = set()
+
+santax = 0
+santay = 0
+robox = 0
+roboy = 0
+new_visited = set()
+
+santas_turn = True
 
 def gen_ident():
     return '%d,%d' % (xpos, ypos)
@@ -21,9 +29,44 @@ def visit(ch):
         xpos -= 1
     visited.add(gen_ident())
 
+def santa_visit(ch):
+    global santax, santay, santas_turn
+    if ch == '^':
+        santay += 1
+    elif ch == '>':
+        santax += 1
+    elif ch == 'v':
+        santay -= 1
+    elif ch == '<':
+        santax -= 1
+    new_visited.add('%d,%d' % (santax, santay))
+    santas_turn = False
+
+def robo_visit(ch):
+    global robox, roboy, santas_turn
+    if ch == '^':
+        roboy += 1
+    elif ch == '>':
+        robox += 1
+    elif ch == 'v':
+        roboy -= 1
+    elif ch == '<':
+        robox -= 1
+    new_visited.add('%d,%d' % (robox, roboy))
+    santas_turn = True
+
 #add starting house
 visited.add(gen_ident())
+new_visited.add(gen_ident())
+
 for line in sys.stdin:
     for c in line:
+        #part one
         visit(c)
-print('Total houses visited: %d' % len(visited))
+        #part two
+        if santas_turn:
+            santa_visit(c)
+        else:
+            robo_visit(c)
+print('Part 1: Total houses visited: %d' % len(visited))
+print('Part 2: Total houses visited: %d' % len(new_visited))
