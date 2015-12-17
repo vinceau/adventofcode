2d6e33f30b475104744527f68083bb97d332d7ef
Vince Au <vinceau09@gmail.com>
2015-12-18 10:42:24 +1100

    Simplify matrix counting

--- day6/day6.py
+++ day6/day6.py
@@ -7,6 +7,9 @@ size = 1000
 #part 1
 matrix = [[False for _ in range(size)] for _ in range(size)]
 
+def count(mat):
+    return sum(sum(x) for x in mat)
+
 def turn(onoff, coord1, coord2):
     x1, y1 = [int(x) for x in coord1.split(',')]
     x2, y2 = [int(x) for x in coord2.split(',')]
@@ -20,14 +23,6 @@ def toggle(coord1, coord2):
     for x in range(x1, x2+1):
         for y in range(y1, y2+1):
             matrix[x][y] = not matrix[x][y]
-
-def count():
-    c = 0
-    for x in range(size):
-        for y in range(size):
-            if matrix[x][y]:
-                c += 1
-    return c
 
 #part 2
 matrix2 = [[0 for _ in range(size)] for _ in range(size)]
@@ -51,13 +46,6 @@ def toggle2(coord1, coord2):
         for y in range(y1, y2+1):
             matrix2[x][y] += 2
 
-def count2():
-    c = 0
-    for x in range(size):
-        for y in range(size):
-            c += matrix2[x][y]
-    return c
-
 for line in sys.stdin:
     if len(line.split()) == 5:
         _, onoff, c1, _, c2 = line.split()
@@ -68,5 +56,5 @@ for line in sys.stdin:
         toggle(c1, c2)
         toggle2(c1, c2)
 
-print('Part 1: There are %d lights that are on' % count())
-print('Part 2: The total brightness is %d' % count2())
+print('Part 1: There are %d lights that are on' % count(matrix))
+print('Part 2: The total brightness is %d' % count(matrix2))
